2dd80a9cc09cff7810baf413bb4894e1b87d8aa7
Vasily Shakhov <vasily.shakhov@mlearner.com>
2013-03-20 18:14:42 +0400

    new battery view

--- js/view/shapes/WireBox/BatteryView.js
+++ js/view/shapes/WireBox/BatteryView.js
@@ -16,9 +16,9 @@ define( [
     self.voltage = new Property( 0 );
 
     //sizes of parts of battery
-    //default 53px and 25px, totW = 78px
+    //default 72px and 6px, totW = 78px
     totWidth -= 4;
-    var w = [totWidth * 53 / 78, totWidth * 25 / 78],
+    var w = [totWidth * 72 / 78, totWidth * 6 / 78],
         h = 40;
 
     //middle
@@ -27,42 +27,46 @@ define( [
 
     self.view = new Easel.Container().setTransform( x, y );
 
-    //full voltage view
-    var fullView = new Easel.Shape();
-    fullView.graphics.setStrokeStyle( 1 ).beginStroke( "black" );
-    fullView.graphics.beginLinearGradientFill( ['#656565', "#afafaf", '#1e1e1e'], [0, 0.3, 1], 0, 0, 0, h );
-    fullView.graphics.drawRect( 0, 0, w[0], h );
-    fullView.graphics.beginLinearGradientFill( ['#cc4e00', "#dddad6", '#cc4e00'], [0, 0.3, 1], 0, 0, 0, h );
-    fullView.graphics.drawRect( w[0], 0, w[1], h );
-    fullView.graphics.beginLinearGradientFill( ['#c3c3c3', "#f9f9f9", '#404040'], [0, 0.3, 1], 0, 0, 0, h );
-    fullView.graphics.drawRect( totWidth, 14, 4, 12 );
-    fullView.$text1 = new Easel.Text( "1.5", "16px Verdana bold" ).setTransform( 3, 3 );
-    fullView.$text2 = new Easel.Text( "V", "18px Verdana bold", "blue" ).setTransform( 35, 1 );
+    //battery view
+    var batView = new Easel.Shape();
+    batView.$text1 = new Easel.Text( "1.5", "16px Verdana bold" ).setTransform( 3, 3 );
+    batView.$text2 = new Easel.Text( "V", "18px Verdana bold", "blue" ).setTransform( 35, 1 );
+    batView.$text3 = new Easel.Text( "", "16px Verdana bold" ).setTransform( 3, -20 );
+    batView.$text4 = new Easel.Text( "V", "18px Verdana bold", "blue" ).setTransform( 34, -23 );
 
+    //w1 - width of first part(grey) of battery
+    var drawBattery = function ( w1 ) {
+      batView.graphics.clear().setStrokeStyle( 1 ).beginStroke( "black" );
+      batView.graphics.beginLinearGradientFill( ['#656565', "#afafaf", '#1e1e1e'], [0, 0.3, 1], 0, 0, 0, h );
+      batView.graphics.drawRect( 0, 0, w1, h );
+      batView.graphics.beginLinearGradientFill( ['#cc4e00', "#dddad6", '#cc4e00'], [0, 0.3, 1], 0, 0, 0, h );
+      batView.graphics.drawRect( w1, 0, w[1], h );
+      batView.graphics.beginLinearGradientFill( ['#c3c3c3', "#f9f9f9", '#404040'], [0, 0.3, 1], 0, 0, 0, h );
+      batView.graphics.drawRect( w1 + w[1], 14, 4, 12 );
+    };
+
+    //partView when voltage 1.5
     var setFull = function () {
       self.view.removeAllChildren();
-      self.view.addChild( fullView );
-      self.view.addChild( fullView.$text1 );
-      self.view.addChild( fullView.$text2 );
+      drawBattery( w[0] );
+      self.view.addChild( batView );
+      self.view.addChild( batView.$text1 );
+      self.view.addChild( batView.$text2 );
     };
 
     //partView when voltage not 1.5
-    var partView = new Easel.Shape();
-    partView.$text1 = new Easel.Text( "", "16px Verdana" ).setTransform( 3, -20 );
-
     var setPart = function ( pct ) {
-      var cWidth = pct * totWidth;
-      partView.graphics.clear().setStrokeStyle( 1 ).beginStroke( "black" );
-      partView.graphics.beginLinearGradientFill( ['#c3c3c3', "#f9f9f9", '#404040'], [0, 0.3, 1], 0, 0, 0, h );
-      partView.graphics.drawRect( 0, 0, cWidth, h );
-      partView.graphics.drawRect( cWidth, 14, 4, 12 );
+      var cWidth = pct * totWidth - 6;
+      drawBattery( cWidth );
 
-      partView.$text1.text = Math.round( 15 * pct ) / 10 + " V";
+      batView.$text3.text = (Math.round( 15 * pct ) / 10).toFixed( 1 );
 
       self.view.removeAllChildren();
-      self.view.addChild( partView );
-      self.view.addChild( partView.$text1 );
+      self.view.addChild( batView );
+      self.view.addChild( batView.$text3 );
+      self.view.addChild( batView.$text4 );
     };
+
     self.voltage.addObserver( function ( val ) {
       if ( val === 0 ) {
         self.view.visible = false;
