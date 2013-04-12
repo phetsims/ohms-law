67508c19898c5c8b794dc9d71d781c8bd156ffb8
Vasily Shakhov <vasily.shakhov@mlearner.com>
2013-04-12 12:06:25 +0400

    fixed issue #10

--- js/view/shapes/WireBox/CurrentValueBox.js
+++ js/view/shapes/WireBox/CurrentValueBox.js
@@ -14,18 +14,10 @@ define( [
   return function ( model, x, y, w, h ) {
     var root = new Easel.Container();
 
-    //rect around text
-    var rectW = 350,
-        rectH = 70,
-        rectX = w / 2 + x - rectW / 2,
-        rectY = h / 2 + y - rectH / 2;
-
-    root.addChild( new WhiteBox( rectX, rectY, rectW, rectH ) );
+    var maxW = 0.9 * w;
 
     //text size and y point of texts
-    var textSize = 34,
-        midY = rectY + rectH / 2 - 1.25 * textSize / 2,
-        midX = rectX + rectW / 2;
+    var textSize = 34;
 
     //texts parts of full string
     var texts = [
@@ -38,7 +30,7 @@ define( [
         color: "black"
       },
       {
-          val: "1004",
+        val: "1004",
         color: "black"
       },
       {
@@ -48,29 +40,43 @@ define( [
     ];
 
     //init and transform texts
-
+    var textContainer = new Easel.Container();
     var totW = 0;
     texts.forEach( function ( entry ) {
-      entry.view = new Easel.Text( entry.val, textSize + "px Verdana", entry.color );
-      root.addChild( entry.view );
+      entry.view = new Easel.Text( entry.val, textSize + "px Verdana", entry.color ).setTransform( totW, 0 );
+      textContainer.addChild( entry.view );
       entry.width = entry.view.getMeasuredWidth();
       totW += entry.width;
     } );
 
-    var offset = midX - totW / 2;
-    texts.forEach( function ( entry ) {
-      entry.view.setTransform( offset, midY );
-      offset += entry.width;
-    } );
+    //scale
+    var scale = 1;
+    if ( totW > maxW ) {
+      scale = maxW / totW;
+    }
 
     texts[2].view.textAlign = "end";
-    texts[2].view.setTransform( texts[2].view.x + texts[2].width, midY );
+    texts[2].view.setTransform( texts[2].view.x + texts[2].width, 0 );
 
     //observer, changes view when current value changes
     var setCurrentText = function ( val ) {
       texts[2].view.text = val;
     };
 
+    //rect around text
+
+    var rectW = totW * scale,
+        rectH = 70,
+        rectX = w / 2 + x - rectW / 2,
+        rectY = h / 2 + y - rectH / 2;
+
+    var midY = rectY + rectH / 2 - scale * 1.25 * textSize / 2,
+        midX = rectX + rectW / 2,
+        offset = midX - scale * totW / 2;
+
+    root.addChild( new WhiteBox( rectX - 10, rectY, rectW + 20, rectH ) );
+    root.addChild( textContainer );
+    textContainer.setTransform( offset, midY, scale, scale );
     model.current.addObserver( setCurrentText );
     return root;
   };
