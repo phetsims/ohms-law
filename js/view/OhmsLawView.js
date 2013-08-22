79539999076ff75c02a1826482652cca60b30581
Chris Malley <cmalley@pixelzoom.com>
2013-08-22 15:55:18 -0600

    joist#35, terminology change: tab -> screen

--- js/view/OhmsLawView.js
+++ js/view/OhmsLawView.js
@@ -1,6 +1,6 @@
 /**
  * Copyright 2002-2013, University of Colorado
- * View for OhmsLaw simulations. main TabView, main stage
+ * View for OhmsLaw simulations.
  * @author Vasily Shakhov (Mlearner)
  * @author Anton Ulyanov (Mlearner)
  */
@@ -8,16 +8,16 @@
 define( function( require ) {
   'use strict';
   var OhmsLawStage = require( 'view/OhmsLawStage' );
-  var TabView = require( 'JOIST/TabView' );
+  var ScreenView = require( 'JOIST/ScreenView' );
   var inherit = require( 'PHET_CORE/inherit' );
 
   function OhmsLawView( model ) {
-    TabView.call( this, { renderer: 'svg' } );
+    ScreenView.call( this, { renderer: 'svg' } );
     //main stage
     this.addChild( new OhmsLawStage( model ) );
   }
 
-  inherit( TabView, OhmsLawView );
+  inherit( ScreenView, OhmsLawView );
   return OhmsLawView;
 
 } )
