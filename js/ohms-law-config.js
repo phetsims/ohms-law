e88aebe37243a6fa83ebcc538a1e4c00da1a9b3a
samreid <reids@colorado.edu>
2013-09-23 20:56:44 -0600

    updated comments

--- js/ohms-law-config.js
+++ js/ohms-law-config.js
@@ -16,7 +16,8 @@ require.config( {
   },
 
   paths: {
-    // third-party libs
+
+    //plugins
     i18n: '../../sherpa/i18n-2.0.4',
     image: '../../chipper/requirejs-plugins/image',
     audio: '../../chipper/requirejs-plugins/audio',
