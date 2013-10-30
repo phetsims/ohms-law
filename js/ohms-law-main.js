5094ef9ba10127c856d9f5e585f22847a2e1f801
Chris Malley <cmalley@pixelzoom.com>
2013-10-29 23:14:26 -0600

    joist#66, generalize handling of credits, including i18n

--- js/ohms-law-main.js
+++ js/ohms-law-main.js
@@ -16,14 +16,15 @@ define( function( require ) {
     OhmsLawView = require( 'view/OhmsLawView' );
 
   var simOptions = {
-    credits: 'PhET Development Team -\n' +
-             'Lead Design: Michael Dubson\n' +
-             'Software Development: Michael Dubson, John Blanco\n' +
-             'Design Team: Ariel Paul\n' +
-             'Interviews: Mindy Gratny\n',
-    thanks: 'Thanks -\n' +
-            'Thanks to Mobile Learner Labs for working with the PhET development team to convert this simulation to HTML5.'
+    credits: {
+      leadDesign: 'Michael Dubson',
+      softwareDevelopment: 'Michael Dubson, John Blanco',
+      designTeam: 'Ariel Paul',
+      interviews: 'Mindy Gratny',
+      thanks: 'Thanks to Mobile Learner Labs for working with the PhET development team\nto convert this simulation to HTML5.'
+    }
   };
+
   SimLauncher.launch( function() {
     //Create and start the sim
     new Sim( simTitle, [
