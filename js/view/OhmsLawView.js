b298b7311dca9caf453395261c00de5c82466e67
Chris Malley <cmalley@pixelzoom.com>
2014-12-22 14:42:07 -0700

    phetsims/joist#126 ohms-law: explicitly set layoutBounds to old joist default

--- js/view/OhmsLawView.js
+++ js/view/OhmsLawView.js
@@ -7,12 +7,15 @@
 
 define( function( require ) {
   'use strict';
+
+  // modules
+  var Bounds2 = require( 'DOT/Bounds2' );
   var OhmsLawStage = require( 'view/OhmsLawStage' );
   var ScreenView = require( 'JOIST/ScreenView' );
   var inherit = require( 'PHET_CORE/inherit' );
 
   function OhmsLawView( model ) {
-    ScreenView.call( this, { renderer: 'svg' } );
+    ScreenView.call( this, { renderer: 'svg', layoutBounds: new Bounds2( 0, 0, 768, 504 ) } );
     //main stage
     this.addChild( new OhmsLawStage( model ) );
   }
