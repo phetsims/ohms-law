844107b32c2ab26995e26cec40f08779257d2b0e
zepumph <michael.kauzmann@colorado.edu>
2018-12-18 07:55:33 -0900

    Control Panel -> Control Area, https://github.com/phetsims/a11y-research/issues/110

--- js/ohms-law/OhmsLawA11yStrings.js
+++ js/ohms-law/OhmsLawA11yStrings.js
@@ -164,7 +164,7 @@ define( function( require ) {
     summarySim: {
       value: 'In the Play Area you find the equation for Ohm\'s Law, <strong>V</strong> equals ' +
              '<strong>I</strong> times <strong>R</strong>, and a ' + 'circuit. Voltage and resistance sliders ' +
-             'allow changes to the equaltion and circuit. In the Control Panel, buttons mute sound or reset the sim.'
+             'allow changes to the equaltion and circuit. In the Control Area, buttons mute sound or reset the sim.'
     },
 
     stateOfSim: {
