5bef1cdd2b423c252bd68691c7f74cc502f71b27
Jesse Greenberg <jesse.greenberg@colorado.edu>
2019-04-05 08:49:25 -0600

    reename GeneralNavigationHelpContent -> GeneralKeyboardHelpSection, see phetsims/scenery-phet#478

--- js/ohms-law/view/OhmsLawKeyboardHelpContent.js
+++ js/ohms-law/view/OhmsLawKeyboardHelpContent.js
@@ -10,7 +10,7 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var GeneralNavigationHelpContent = require( 'SCENERY_PHET/keyboard/help/GeneralNavigationHelpContent' );
+  var GeneralKeyboardHelpSection = require( 'SCENERY_PHET/keyboard/help/GeneralKeyboardHelpSection' );
   var HBox = require( 'SCENERY/nodes/HBox' );
   var inherit = require( 'PHET_CORE/inherit' );
   var ohmsLaw = require( 'OHMS_LAW/ohmsLaw' );
@@ -22,7 +22,7 @@ define( function( require ) {
   function OhmsLawKeyboardHelpContent() {
 
     var sliderControlsHelpContent = new SliderControlsHelpContent();
-    var generalNavigationHelpContent = new GeneralNavigationHelpContent();
+    var generalNavigationHelpContent = new GeneralKeyboardHelpSection();
 
     HBox.call( this, {
       children: [ sliderControlsHelpContent, generalNavigationHelpContent ],
