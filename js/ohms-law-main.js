6c9c545a774624300a03c1be01654cd042d08346
Jesse Greenberg <jesse.greenberg@colorado.edu>
2019-05-24 17:58:21 -0400

    update credits for #54

--- js/ohms-law-main.js
+++ js/ohms-law-main.js
@@ -27,9 +27,9 @@ define( function( require ) {
   var simOptions = {
     credits: {
       leadDesign: 'Michael Dubson',
-      softwareDevelopment: 'Michael Dubson, John Blanco',
-      team: 'Mindy Gratny, Ariel Paul',
-      qualityAssurance: 'Steele Dalton, Bryce Griebenow, Elise Morgan, Oliver Orejola, Benjamin Roberts, Bryan Yoelin',
+      softwareDevelopment: 'Michael Dubson, John Blanco, Michael Kauzmann, Martin Veillette',
+      team: 'Mindy Gratny, Emily Moore, Ariel Paul, Kathy Perkins, Taliesin Smith',
+      qualityAssurance: 'Steele Dalton, Alex Dornan, Bryce Griebenow, Ethan Johnson, Elise Morgan, Liam Mulhall, Oliver Orejola, Benjamin Roberts, Ethan Ward, Kathryn Woessner, Bryan Yoelin',
       thanks: 'Thanks to Mobile Learner Labs for working with the PhET development team to convert this ' +
               'simulation to HTML5.'
     },
