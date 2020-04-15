44649432460a8b4a425d815fe8ca20c3b46e10d2
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-15 19:30:32 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/628

--- js/phet-io/ohms-law-baseline.js
+++ js/phet-io/ohms-law-baseline.js
@@ -1795,7 +1795,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
-    "ohmsLaw.general.view.navigationBar.titleTextNode": {
+    "ohmsLaw.general.view.navigationBar.titleText": {
       "phetioDocumentation": "Displays the title of the simulation in the navigation bar (bottom left)",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
@@ -1808,7 +1808,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "TextIO"
     },
-    "ohmsLaw.general.view.navigationBar.titleTextNode.opacityProperty": {
+    "ohmsLaw.general.view.navigationBar.titleText.opacityProperty": {
       "phetioDocumentation": "Opacity of the parent NodeIO, between 0 (invisible) and 1 (fully visible)",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
@@ -1821,7 +1821,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "NumberPropertyIO"
     },
-    "ohmsLaw.general.view.navigationBar.titleTextNode.pickableProperty": {
+    "ohmsLaw.general.view.navigationBar.titleText.pickableProperty": {
       "phetioDocumentation": "Sets whether the node will be pickable (and hence interactive), see the NodeIO documentation for more details",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
@@ -1834,7 +1834,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<NullableIO<BooleanIO>>"
     },
-    "ohmsLaw.general.view.navigationBar.titleTextNode.textProperty": {
+    "ohmsLaw.general.view.navigationBar.titleText.textProperty": {
       "phetioDocumentation": "Property for the displayed text",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
@@ -1847,7 +1847,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<StringIO>"
     },
-    "ohmsLaw.general.view.navigationBar.titleTextNode.visibleProperty": {
+    "ohmsLaw.general.view.navigationBar.titleText.visibleProperty": {
       "phetioDocumentation": "Controls whether the Node will be visible (and interactive), see the NodeIO documentation for more details.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
