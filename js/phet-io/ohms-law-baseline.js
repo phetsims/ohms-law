c04301174af0e1f84d5f292620dc69e2bf509b26
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-27 17:02:04 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/628

--- js/phet-io/ohms-law-baseline.js
+++ js/phet-io/ohms-law-baseline.js
@@ -1859,7 +1859,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
-    "ohmsLaw.general.view.simNameProperty": {
+    "ohmsLaw.general.model.simNameProperty": {
       "phetioDocumentation": "The name of the sim. Changing this value will update the title text on the navigation bar and the title text on the home screen, if it exists.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
