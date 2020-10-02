0834cb865982a9b5f2a61dc0518f95c2126d757a
pixelzoom <cmalley@pixelzoom.com>
2020-10-01 20:48:14 -0600

    convert Panel subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/ohms-law/view/ControlPanel.js
+++ js/ohms-law/view/ControlPanel.js
@@ -7,7 +7,6 @@
  */
 
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import merge from '../../../../phet-core/js/merge.js';
 import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
 import MathSymbols from '../../../../scenery-phet/js/MathSymbols.js';
@@ -15,8 +14,8 @@ import HBox from '../../../../scenery/js/nodes/HBox.js';
 import Panel from '../../../../sun/js/Panel.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import ValueChangeUtterance from '../../../../utterance-queue/js/ValueChangeUtterance.js';
-import ohmsLawStrings from '../../ohmsLawStrings.js';
 import ohmsLaw from '../../ohmsLaw.js';
+import ohmsLawStrings from '../../ohmsLawStrings.js';
 import OhmsLawModel from '../model/OhmsLawModel.js';
 import OhmsLawA11yStrings from '../OhmsLawA11yStrings.js';
 import OhmsLawConstants from '../OhmsLawConstants.js';
@@ -44,144 +43,144 @@ const aLotString = OhmsLawA11yStrings.aLot.value;
 // constants
 const NUMBER_OF_LETTER_SIZES = OhmsLawA11yStrings.numberOfSizes.value; // pdom - the number of sizes that letters can be described as.
 
-/**
- * @param {OhmsLawModel} model
- * @param {OhmsLawDescriber} ohmsLawDescriber
- * @param {Object} [options]
- * @constructor
- */
-function ControlPanel( model, ohmsLawDescriber, options ) {
+class ControlPanel extends Panel {
 
-  options = merge( {
-    xMargin: 30,
-    yMargin: 10,
-    lineWidth: 3,
-    resize: false,
-    preventFit: true, // used to avoid jostling in the control panel when the resistance changes quickly, see https://github.com/phetsims/ohms-law/issues/68
-    tandem: Tandem.REQUIRED
-  }, options );
+  /**
+   * @param {OhmsLawModel} model
+   * @param {OhmsLawDescriber} ohmsLawDescriber
+   * @param {Object} [options]
+   */
+  constructor( model, ohmsLawDescriber, options ) {
 
-  const voltageProperty = model.voltageProperty;
-  const resistanceProperty = model.resistanceProperty;
-  const currentProperty = model.currentProperty;
+    options = merge( {
+      xMargin: 30,
+      yMargin: 10,
+      lineWidth: 3,
+      resize: false,
+      preventFit: true, // used to avoid jostling in the control panel when the resistance changes quickly, see https://github.com/phetsims/ohms-law/issues/68
+      tandem: Tandem.REQUIRED
+    }, options );
 
-  // pdom - to alert changes to assistive devices
-  const resistanceUtterance = new ValueChangeUtterance();
-  const voltageUtterance = new ValueChangeUtterance();
+    const voltageProperty = model.voltageProperty;
+    const resistanceProperty = model.resistanceProperty;
+    const currentProperty = model.currentProperty;
 
-  // Create the voltage slider with readout and labels
-  let oldVoltage; // stored on startDrag;
-  let newVoltage; // stored on endDrag;
-  const voltageSlider = new SliderUnit(
-    voltageProperty,
-    OhmsLawConstants.VOLTAGE_RANGE,
-    voltageSymbolString,
-    voltageString,
-    voltageUnitsString,
-    voltageSliderLabelString,
-    {
-      sliderOptions: {
+    // pdom - to alert changes to assistive devices
+    const resistanceUtterance = new ValueChangeUtterance();
+    const voltageUtterance = new ValueChangeUtterance();
 
-        // pdom
-        keyboardStep: 0.5, // volts
-        a11yCreateAriaValueText: value => StringUtils.fillIn( voltageUnitsPatternString, { value: value } ),
-        startDrag: function() {
-          oldVoltage = voltageProperty.get();
-        },
-        endDrag: function() {
-          newVoltage = voltageProperty.get();
+    // Create the voltage slider with readout and labels
+    let oldVoltage; // stored on startDrag;
+    let newVoltage; // stored on endDrag;
+    const voltageSlider = new SliderUnit(
+      voltageProperty,
+      OhmsLawConstants.VOLTAGE_RANGE,
+      voltageSymbolString,
+      voltageString,
+      voltageUnitsString,
+      voltageSliderLabelString,
+      {
+        sliderOptions: {
 
-          if ( oldVoltage !== newVoltage ) {
-            // pdom - when V changes, announce an alert that describes the change
-            const sizeChange = newVoltage - oldVoltage > 0 ? growsString : shrinksString;
-            voltageUtterance.alert = ohmsLawDescriber.getValueChangeAlertString( letterVString, sizeChange, sizeChange );
-            phet.joist.sim.utteranceQueue.addToBack( voltageUtterance );
+          // pdom
+          keyboardStep: 0.5, // volts
+          a11yCreateAriaValueText: value => StringUtils.fillIn( voltageUnitsPatternString, { value: value } ),
+          startDrag: function() {
+            oldVoltage = voltageProperty.get();
+          },
+          endDrag: function() {
+            newVoltage = voltageProperty.get();
+
+            if ( oldVoltage !== newVoltage ) {
+              // pdom - when V changes, announce an alert that describes the change
+              const sizeChange = newVoltage - oldVoltage > 0 ? growsString : shrinksString;
+              voltageUtterance.alert = ohmsLawDescriber.getValueChangeAlertString( letterVString, sizeChange, sizeChange );
+              phet.joist.sim.utteranceQueue.addToBack( voltageUtterance );
+            }
           }
-        }
-      },
-
-      decimalPlaces: OhmsLawConstants.VOLTAGE_SIG_FIGS,
-      tandem: options.tandem.createTandem( 'voltageSlider' )
-    }
-  );
-
-  let oldResistance; // stored on startDrag
-  let newResistance; // stored on endDrag
-  let oldCurrent;
-  let newCurrent;
-
-  // based on the number of sizes for the formula letters
-  const currentRangePerSize = ( OhmsLawModel.getCurrentRange().max - OhmsLawModel.getCurrentRange().min ) / NUMBER_OF_LETTER_SIZES;
-  const twoSizeCurrentThreshhold = currentRangePerSize * 2; // amount of current that must change to adjust change the current 2 a11y sizes.
-
-  // pdom - This function will create the string alert to notify the resistance slider has been changed.
-  const endResistanceDrag = function() {
-    newResistance = resistanceProperty.get();
-    newCurrent = currentProperty.get();
-
-    if ( newResistance !== oldResistance ) {
-      const resistanceChange = newResistance - oldResistance;
-      const currentChange = newCurrent - oldCurrent;
-
-      const rSizeChange = resistanceChange > 0 ? growsString : shrinksString;
-      let iSizeChange = resistanceChange < 0 ? growsString : shrinksString;
-      iSizeChange += Math.abs( currentChange ) > twoSizeCurrentThreshhold ? ' ' + aLotString : '';
-
-      resistanceUtterance.alert = ohmsLawDescriber.getValueChangeAlertString( letterRString, rSizeChange, iSizeChange );
-      phet.joist.sim.utteranceQueue.addToBack( resistanceUtterance );
-    }
-  };
-
-  // Create the resistance slider with readout and labels
-  const resistanceSlider = new SliderUnit(
-    resistanceProperty,
-    OhmsLawConstants.RESISTANCE_RANGE,
-    resistanceSymbolString,
-    resistanceString,
-    MathSymbols.OHMS,
-    resistanceSliderLabelString,
-    {
-      sliderOptions: {
-
-        // pdom
-        keyboardStep: 20, // ohms
-        shiftKeyboardStep: 1, // ohms
-        a11yCreateAriaValueText: value => StringUtils.fillIn( resistanceUnitsPatternString, { value: value } ),
-        startDrag: function() {
-          oldResistance = resistanceProperty.get();
-          oldCurrent = currentProperty.get();
         },
-        endDrag: endResistanceDrag
-      },
-      decimalPlaces: OhmsLawConstants.RESISTANCE_SIG_FIGS,
-      tandem: options.tandem.createTandem( 'resistanceSlider' )
+
+        decimalPlaces: OhmsLawConstants.VOLTAGE_SIG_FIGS,
+        tandem: options.tandem.createTandem( 'voltageSlider' )
+      }
+    );
+
+    let oldResistance; // stored on startDrag
+    let newResistance; // stored on endDrag
+    let oldCurrent;
+    let newCurrent;
+
+    // based on the number of sizes for the formula letters
+    const currentRangePerSize = ( OhmsLawModel.getCurrentRange().max - OhmsLawModel.getCurrentRange().min ) / NUMBER_OF_LETTER_SIZES;
+    const twoSizeCurrentThreshhold = currentRangePerSize * 2; // amount of current that must change to adjust change the current 2 a11y sizes.
+
+    // pdom - This function will create the string alert to notify the resistance slider has been changed.
+    const endResistanceDrag = function() {
+      newResistance = resistanceProperty.get();
+      newCurrent = currentProperty.get();
+
+      if ( newResistance !== oldResistance ) {
+        const resistanceChange = newResistance - oldResistance;
+        const currentChange = newCurrent - oldCurrent;
+
+        const rSizeChange = resistanceChange > 0 ? growsString : shrinksString;
+        let iSizeChange = resistanceChange < 0 ? growsString : shrinksString;
+        iSizeChange += Math.abs( currentChange ) > twoSizeCurrentThreshhold ? ' ' + aLotString : '';
+
+        resistanceUtterance.alert = ohmsLawDescriber.getValueChangeAlertString( letterRString, rSizeChange, iSizeChange );
+        phet.joist.sim.utteranceQueue.addToBack( resistanceUtterance );
+      }
+    };
+
+    // Create the resistance slider with readout and labels
+    const resistanceSlider = new SliderUnit(
+      resistanceProperty,
+      OhmsLawConstants.RESISTANCE_RANGE,
+      resistanceSymbolString,
+      resistanceString,
+      MathSymbols.OHMS,
+      resistanceSliderLabelString,
+      {
+        sliderOptions: {
+
+          // pdom
+          keyboardStep: 20, // ohms
+          shiftKeyboardStep: 1, // ohms
+          a11yCreateAriaValueText: value => StringUtils.fillIn( resistanceUnitsPatternString, { value: value } ),
+          startDrag: function() {
+            oldResistance = resistanceProperty.get();
+            oldCurrent = currentProperty.get();
+          },
+          endDrag: endResistanceDrag
+        },
+        decimalPlaces: OhmsLawConstants.RESISTANCE_SIG_FIGS,
+        tandem: options.tandem.createTandem( 'resistanceSlider' )
+      } );
+
+    // Use a content node so that the Panel can surround it fully
+    const content = new HBox( {
+      spacing: 30, // empirically determined
+      children: [ voltageSlider, resistanceSlider ],
+
+      // pdom - contain the sliders in a list
+      labelTagName: 'h3',
+      tagName: 'div',
+      labelContent: sliderControlsString,
+      descriptionContent: slidersDescriptionString
     } );
 
-  // Use a content node so that the Panel can surround it fully
-  const content = new HBox( {
-    spacing: 30, // empirically determined
-    children: [ voltageSlider, resistanceSlider ],
+    super( content, options );
 
-    // pdom - contain the sliders in a list
-    labelTagName: 'h3',
-    tagName: 'div',
-    labelContent: sliderControlsString,
-    descriptionContent: slidersDescriptionString
-  } );
-
-  // @public (read-only) {BooleanProperty} - a property that indicates whether either slider is being dragged via
-  // keyboard interaction
-  this.sliderBeingDraggedByKeyboardProperty = new DerivedProperty(
-    [ voltageSlider.sliderDraggingByKeyboardProperty, resistanceSlider.sliderDraggingByKeyboardProperty ],
-    function( voltageSliderDraggedByKeyboard, resistanceSliderDraggedByKeyboard ) {
-      return voltageSliderDraggedByKeyboard || resistanceSliderDraggedByKeyboard;
-    }
-  );
-
-  Panel.call( this, content, options );
+    // @public (read-only) {DerivedProperty.<boolean>} - a property that indicates whether either slider is being
+    // dragged via keyboard interaction
+    this.sliderBeingDraggedByKeyboardProperty = new DerivedProperty(
+      [ voltageSlider.sliderDraggingByKeyboardProperty, resistanceSlider.sliderDraggingByKeyboardProperty ],
+      function( voltageSliderDraggedByKeyboard, resistanceSliderDraggedByKeyboard ) {
+        return voltageSliderDraggedByKeyboard || resistanceSliderDraggedByKeyboard;
+      }
+    );
+  }
 }
 
 ohmsLaw.register( 'ControlPanel', ControlPanel );
-
-inherit( Panel, ControlPanel );
 export default ControlPanel;
--- js/ohms-law/view/ReadoutPanel.js
+++ js/ohms-law/view/ReadoutPanel.js
@@ -8,7 +8,6 @@
 
 import Property from '../../../../axon/js/Property.js';
 import Utils from '../../../../dot/js/Utils.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import merge from '../../../../phet-core/js/merge.js';
 import PhetColorScheme from '../../../../scenery-phet/js/PhetColorScheme.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
@@ -17,8 +16,8 @@ import Rectangle from '../../../../scenery/js/nodes/Rectangle.js';
 import Text from '../../../../scenery/js/nodes/Text.js';
 import Panel from '../../../../sun/js/Panel.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
-import ohmsLawStrings from '../../ohmsLawStrings.js';
 import ohmsLaw from '../../ohmsLaw.js';
+import ohmsLawStrings from '../../ohmsLawStrings.js';
 import CurrentUnit from '../model/CurrentUnit.js';
 import OhmsLawModel from '../model/OhmsLawModel.js';
 import OhmsLawConstants from '../OhmsLawConstants.js';
@@ -31,80 +30,78 @@ const currentUnitsString = ohmsLawStrings.currentUnits;
 const FONT = new PhetFont( 32 );
 const MAX_READOUT_WIDTH = 0.63 * OhmsLawConstants.WIRE_WIDTH;
 
-/**
- * @param {OhmsLawModel} model
- * @param {Tandem} tandem
- * @param {Object} [options]
- * @constructor
- */
-function ReadoutPanel( model, options ) {
+class ReadoutPanel extends Panel {
 
-  options = merge( {
-    xMargin: 30,
-    yMargin: 8,
-    lineWidth: 3,
-    resize: false,
-    tandem: Tandem.REQUIRED
-  }, options );
+  /**
+   * @param {OhmsLawModel} model
+   * @param {Object} [options]
+   */
+  constructor( model, options ) {
 
-  const currentStringText = new Text( currentString, {
-    font: FONT,
-    fill: PhetColorScheme.RED_COLORBLIND,
-    maxWidth: 110, // empirically determined
-    tandem: options.tandem.createTandem( 'currentStringText' )
-  } );
+    options = merge( {
+      xMargin: 30,
+      yMargin: 8,
+      lineWidth: 3,
+      resize: false,
+      tandem: Tandem.REQUIRED
+    }, options );
 
-  const equalsSign = new Text( '=', {
-    font: FONT,
-    fill: 'black',
-    tandem: options.tandem.createTandem( 'equalsSign' ),
-    phetioReadOnly: true
-  } );
+    const currentStringText = new Text( currentString, {
+      font: FONT,
+      fill: PhetColorScheme.RED_COLORBLIND,
+      maxWidth: 110, // empirically determined
+      tandem: options.tandem.createTandem( 'currentStringText' )
+    } );
 
-  const currentValue = new Text( Utils.toFixed( OhmsLawModel.getMaxCurrent(), 1 ), {
-    font: FONT,
-    fill: 'black',
-    tandem: options.tandem.createTandem( 'currentValue' ),
-    phetioReadOnly: true
-  } );
+    const equalsSign = new Text( '=', {
+      font: FONT,
+      fill: 'black',
+      tandem: options.tandem.createTandem( 'equalsSign' ),
+      phetioReadOnly: true
+    } );
 
-  // To keep the correct spacing as the current value changes
-  const currentValueBackground = new Rectangle.bounds( currentValue.bounds, {
-    children: [ currentValue ]
-  } );
+    const currentValue = new Text( Utils.toFixed( OhmsLawModel.getMaxCurrent(), 1 ), {
+      font: FONT,
+      fill: 'black',
+      tandem: options.tandem.createTandem( 'currentValue' ),
+      phetioReadOnly: true
+    } );
 
-  const currentUnit = new Text( currentUnitsString, {
-    font: FONT,
-    fill: PhetColorScheme.RED_COLORBLIND,
-    tandem: options.tandem.createTandem( 'currentUnit' ),
-    phetioReadOnly: true
-  } );
+    // To keep the correct spacing as the current value changes
+    const currentValueBackground = new Rectangle.bounds( currentValue.bounds, {
+      children: [ currentValue ]
+    } );
 
-  const textContainer = new HBox( {
-    spacing: 11.3, // empirically determined
-    children: [ currentStringText, equalsSign, currentValueBackground, currentUnit ]
-  } );
+    const currentUnit = new Text( currentUnitsString, {
+      font: FONT,
+      fill: PhetColorScheme.RED_COLORBLIND,
+      tandem: options.tandem.createTandem( 'currentUnit' ),
+      phetioReadOnly: true
+    } );
 
-  // Scale the text if greater than max allowed width.
-  if ( textContainer.width > MAX_READOUT_WIDTH ) {
-    textContainer.scale( MAX_READOUT_WIDTH / textContainer.width );
+    const textContainer = new HBox( {
+      spacing: 11.3, // empirically determined
+      children: [ currentStringText, equalsSign, currentValueBackground, currentUnit ]
+    } );
+
+    // Scale the text if greater than max allowed width.
+    if ( textContainer.width > MAX_READOUT_WIDTH ) {
+      textContainer.scale( MAX_READOUT_WIDTH / textContainer.width );
+    }
+
+    // Present for the lifetime of the simulation, no need to unlink.
+    Property.multilink( [ model.currentProperty,
+      model.currentUnitsProperty ], ( current, units ) => {
+      const rightEdgePosition = currentValue.right;
+      currentValue.text = model.getFixedCurrent();
+      currentValue.right = rightEdgePosition;
+
+      currentUnit.text = units === CurrentUnit.AMPS ? currentAmpUnitsString : currentUnitsString;
+    } );
+
+    super( textContainer, options );
   }
-
-  // Present for the lifetime of the simulation, no need to unlink.
-  Property.multilink( [ model.currentProperty,
-    model.currentUnitsProperty ], ( current, units ) => {
-    const rightEdgePosition = currentValue.right;
-    currentValue.text = model.getFixedCurrent();
-    currentValue.right = rightEdgePosition;
-
-    currentUnit.text = units === CurrentUnit.AMPS ? currentAmpUnitsString : currentUnitsString;
-  } );
-
-  // Create the panel to surround the hBox.
-  Panel.call( this, textContainer, options );
 }
 
 ohmsLaw.register( 'ReadoutPanel', ReadoutPanel );
-
-inherit( Panel, ReadoutPanel );
 export default ReadoutPanel;
