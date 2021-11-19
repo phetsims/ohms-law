784edcee654499fdee607c3f32c11b55cbc1e0fd
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:09 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/ohmsLawStrings.ts
@@ -0,0 +1,32 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import ohmsLaw from './ohmsLaw.js';
+
+type StringsType = {
+  'ohms-law': {
+    'title': string
+  },
+  'current': string,
+  'resistance': string,
+  'voltage': string,
+  'voltageSymbol': string,
+  'resistanceSymbol': string,
+  'currentSymbol': string,
+  'voltageUnits': string,
+  'currentUnits': string,
+  'currentAmpUnits': string,
+  'units': string,
+  'milliampsMA': string,
+  'ampsA': string
+};
+
+const ohmsLawStrings = getStringModule( 'OHMS_LAW' ) as StringsType;
+
+ohmsLaw.register( 'ohmsLawStrings', ohmsLawStrings );
+
+export default ohmsLawStrings;
