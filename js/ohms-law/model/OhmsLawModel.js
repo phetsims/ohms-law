94ccb3b619b0cd3ffcaa176104ebf50fe692a45f
phet-dev <phetadmin@gmail.com>
2022-01-12 03:27:19 -0700

    update copyright dates from daily grunt work

--- js/ohms-law/model/OhmsLawModel.js
+++ js/ohms-law/model/OhmsLawModel.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2021, University of Colorado Boulder
+// Copyright 2013-2022, University of Colorado Boulder
 
 /**
  * Primary model for the Ohm's Law simulation, see doc/model.md for more information.
