68430b799bbaf7cd3f8218a187cacfadff57a003
phet-dev <phetadmin@gmail.com>
2022-05-24 03:51:16 -0600

    update copyright dates from daily grunt work

--- js/ohms-law/view/OhmsLawScreenSummaryNode.js
+++ js/ohms-law/view/OhmsLawScreenSummaryNode.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2021, University of Colorado Boulder
+// Copyright 2017-2022, University of Colorado Boulder
 
 /**
  * The scene accessible summary content for Ohms Law.  This manages the strings available
