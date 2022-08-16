8be35cffc9e0fc95eb01f5ea447dbd0b2595a3d8
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:41 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/ohmsLawStrings.ts
+++ js/ohmsLawStrings.ts
@@ -5,24 +5,38 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import ohmsLaw from './ohmsLaw.js';
 
 type StringsType = {
   'ohms-law': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'current': string;
+  'currentProperty': TReadOnlyProperty<string>;
   'resistance': string;
+  'resistanceProperty': TReadOnlyProperty<string>;
   'voltage': string;
+  'voltageProperty': TReadOnlyProperty<string>;
   'voltageSymbol': string;
+  'voltageSymbolProperty': TReadOnlyProperty<string>;
   'resistanceSymbol': string;
+  'resistanceSymbolProperty': TReadOnlyProperty<string>;
   'currentSymbol': string;
+  'currentSymbolProperty': TReadOnlyProperty<string>;
   'voltageUnits': string;
+  'voltageUnitsProperty': TReadOnlyProperty<string>;
   'currentUnits': string;
+  'currentUnitsProperty': TReadOnlyProperty<string>;
   'currentAmpUnits': string;
+  'currentAmpUnitsProperty': TReadOnlyProperty<string>;
   'units': string;
+  'unitsProperty': TReadOnlyProperty<string>;
   'milliampsMA': string;
+  'milliampsMAProperty': TReadOnlyProperty<string>;
   'ampsA': string;
+  'ampsAProperty': TReadOnlyProperty<string>;
 };
 
 const ohmsLawStrings = getStringModule( 'OHMS_LAW' ) as StringsType;
