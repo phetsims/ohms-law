bf98903e228762cb52a3a059f76d43f9635395d7
phet-dev <phetadmin@gmail.com>
2025-02-08 04:09:19 -0700

    update copyright dates from daily grunt work

--- js/ohms-law/view/SliderUnit.js
+++ js/ohms-law/view/SliderUnit.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2024, University of Colorado Boulder
+// Copyright 2017-2025, University of Colorado Boulder
 
 /**
  * Slider unit with a vertical slider, a label above the slider and a readout display below the slider.
